34746025b5f4048085fed63d7455558620cca058
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2026-01-11 21:23:33 +0600

    update

--- upload_posts/src/App.jsx
+++ upload_posts/src/App.jsx
@@ -1,11 +1,19 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import OpenCreate from './sidebar/OpenCreate';
 import { RxCrossCircled } from "react-icons/rx";
+import axios from 'axios';
 
 function App() {
+const [posts,setPosts] = useState([]);
 
+useEffect(()=>{
+  const Posts = axios.get("https://fancygram.vercel.app/posts");
+  Posts.then((response) => {
+    setPosts(response.data);
+  });
+},[posts])
   const [openCreate, setOpenCreate] = useState(false);
   const createOpen = () => {
     setOpenCreate(true);
@@ -23,6 +31,27 @@ function App() {
           <h1>Upload Posts Here</h1>
           <button onClick={createOpen} className='create-btn' type="button">Create</button>
         </div>
+        <div className="post-container">
+          {
+            posts? posts.map((post) => (
+              <div key={post._id} className="post">
+             {
+              post.mediaUrl?    <img className='post-img' src={post.mediaUrl}  alt="Post media" />
+              : <div style={{
+              background:post.style.background,
+              color: post.style.color,
+              fontSize:post.style.fontSize,
+              textAlign:post.style.fontAlign,
+              lineHeight:post.style.lineHeight.toString()+'px'
+              }}>
+                {post.text}
+                </div>
+             }
+                <p>{post.caption}</p>
+              </div>
+            )) : <p>No posts available</p>
+          }
+       </div>
       </div>
       {
         openCreate && 
